9292a77eb74ebeb3244bb6ddd89b89d7a12bf697
Tiago Leite <tiagosleite2015@gmail.com>
2026-07-16 15:36:26 +0100

    test: add product creation test

--- cypress/e2e/api/API_serverest.cy.js
+++ cypress/e2e/api/API_serverest.cy.js
@@ -81,4 +81,97 @@ describe('User List', () => {
 
 });
 
+
+
+
+describe('POST - Create New Product', () => {
+
+    let token;
+    let productId;
+
+    before(() => {
+
+        // Realiza login
+        cy.request({
+            method: 'POST',
+            url: '/login',
+            body: {
+                email: 'fulano@qa.com',
+                password: 'teste'
+            }
+        }).then((response) => {
+
+            expect(response.status).to.eq(200);
+            expect(response.body).to.have.property('authorization');
+
+            token = response.body.authorization;
+        });
+
+    });
+
+    it.only('Should create a new product successfully', () => {
+
+        const product = {
+            nome: faker.commerce.productName(),
+            preco: Number(faker.commerce.price({
+                min: 100,
+                max: 5000,
+                dec: 0
+            })),
+            descricao: faker.commerce.productDescription(),
+            quantidade: faker.number.int({
+                min: 1,
+                max: 100
+            })
+        };
+
+        cy.request({
+            method: 'POST',
+            url: '/produtos',
+            headers: {
+                Authorization: token
+            },
+            body: product
+
+        }).then((response) => {
+
+            expect(response.status).to.eq(201);
+            expect(response.body.message)
+                .to.equal('Cadastro realizado com sucesso');
+
+            expect(response.body).to.have.property('_id');
+
+            productId = response.body._id;
+
+            
+            cy.request({
+                method: 'GET',
+                url: `/produtos/${productId}`
+            }).then((getResponse) => {
+
+                expect(getResponse.status).to.eq(200);
+
+                expect(getResponse.body.nome)
+                    .to.equal(product.nome);
+
+                expect(getResponse.body.preco)
+                    .to.equal(product.preco);
+
+                expect(getResponse.body.descricao)
+                    .to.equal(product.descricao);
+
+                expect(getResponse.body.quantidade)
+                    .to.equal(product.quantidade);
+
+                expect(getResponse.body._id)
+                    .to.equal(productId);
+
+            });
+
+        });
+
+    });
+
+});
+
         
